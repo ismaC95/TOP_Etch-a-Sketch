9fc8133e50a715e35225db4d4908fb0a620ea273
Ismael <ismacm95@gmail.com>
2024-06-28 17:04:23 +0100

    Creating a button that will gather how many rows the user wants and display them after clearing the previous grids

--- javascript.js
+++ javascript.js
@@ -2,15 +2,18 @@ const body = document.querySelector("body");
 const container = document.querySelector("#container");
 
 //creating a grid of 16x16
-function makeDivs() {
-    const divs = 256;
+function makeDivs(rows) {
+    //clearing existing grid
+    container.innerHTML = '';
 
-    for(i = 0; i < divs; i++){
+    const grid = rows * rows;
+    for(i = 0; i < grid; i++){
         const newDiv = document.createElement("div")
         newDiv.classList.add("divSquares");
         container.appendChild(newDiv);
-        
     }
+    containerWidth(rows);
+    hoveringDivs();
 }
 
 //function to change the color of the div that the mouse hovers
@@ -26,5 +29,24 @@ function hoveringDivs() {
     });
 }
 
-makeDivs();
-hoveringDivs();
+//when clicking the button, the grid will come up with as many rows as the user inputs
+function newGrid(){
+    const btn = document.querySelector("button");
+    btn.addEventListener("click", () => {
+        const gridRows = prompt("How many rows?");
+        makeDivs(gridRows);
+        const grid = document.querySelectorAll(".divSquares");
+        grid.forEach(square => {
+            square.style.backgroundColor = "";
+        });
+    });
+
+}
+
+//make that the maximum width of the container is the number of rows inputed by
+//the user by the amount of pixels a square is
+function containerWidth(gridRows){
+    container.style.width = (gridRows * 30) + "px";
+}
+
+newGrid();
