2b7be7bc0eb8f8393b54bbc85abb077962543e90
Ismael <ismacm95@gmail.com>
2024-06-28 17:37:52 +0100

    Fixing design error. Now the grid will always use the same space and the squares will fill it

--- javascript.js
+++ javascript.js
@@ -1,13 +1,13 @@
 const body = document.querySelector("body");
 const container = document.querySelector("#container");
 
-//creating a grid of 16x16
+//creating a grid
 function makeDivs(rows) {
     //clearing existing grid
     container.innerHTML = '';
 
     const grid = rows * rows;
-    for(i = 0; i < grid; i++){
+    for(let i = 0; i < grid; i++){
         const newDiv = document.createElement("div")
         newDiv.classList.add("divSquares");
         container.appendChild(newDiv);
@@ -21,7 +21,9 @@ function hoveringDivs() {
     const grid = document.querySelectorAll(".divSquares");
     grid.forEach(square => {
         square.addEventListener("mouseover", () => {
-            square.style.backgroundColor = "pink";
+            const {red, blue, green} = randomColor();
+            square.style.backgroundColor = `rgb(${red}, ${blue}
+                , ${green})`;
         });
         /*square.addEventListener("mouseout", () =>{
             square.style.backgroundColor="";
@@ -34,11 +36,16 @@ function newGrid(){
     const btn = document.querySelector("button");
     btn.addEventListener("click", () => {
         const gridRows = prompt("How many rows?");
-        makeDivs(gridRows);
-        const grid = document.querySelectorAll(".divSquares");
-        grid.forEach(square => {
-            square.style.backgroundColor = "";
+        if(gridRows < 1 || gridRows > 100){
+            alert("Please, add a number between 1 and 100 to continue")
+        }
+        else{
+            makeDivs(gridRows);
+            const grid = document.querySelectorAll(".divSquares");
+            grid.forEach(square => {
+                square.style.backgroundColor = "";
         });
+        };
     });
 
 }
@@ -46,7 +53,20 @@ function newGrid(){
 //make that the maximum width of the container is the number of rows inputed by
 //the user by the amount of pixels a square is
 function containerWidth(gridRows){
-    container.style.width = (gridRows * 30) + "px";
+    const grid = document.querySelectorAll(".divSquares");
+    grid.forEach(square => {
+    square.style.width = (500 / gridRows) + "px";
+    square.style.heigth = (500 / gridRows) + "px";
+    });
+}
+
+//randomize colors
+function randomColor(){
+    let red = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+
+    return {red, blue, green};
 }
 
 newGrid();
